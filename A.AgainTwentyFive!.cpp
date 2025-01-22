12efb9a99207077cf8735b2bd84c3d0855b9fa1a
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-01-22 20:32:37 +0600

    Add files via upload

---
+++ A.AgainTwentyFive!.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+#define lli long long int
+#define ll long long
+#define l long
+#define F  first
+#define S  second
+#define pb  push_back
+#define vi vector<int>
+#define vvi vector<vi>
+
+using namespace std;
+
+
+
+
+
+const int mod = 1e9 + 7;
+int power( int a , ll b)
+{
+	if ( b == 1)
+		return a;
+	long long c = power( a , b / 2 );
+	if (b % 2)
+		return (c * a * c) % 100;
+	return (c * c) % 100;
+}
+
+void solve() {
+
+	ll n; cin >> n;
+	cout << power(5, n) << endl;
+
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t = 1;
+	// cin >> t;
+	while (t--) {
+		solve();
+
+	}
+
+	return 0;
+}
